4976e56e5031ffb7aa461ee63e75cbd355c29f95
Dex Fitch <fitch.dexter@gmail.com>
2021-01-30 19:19:54 -0800

    Add serializer to controllers

--- palette-swap-backend/app/controllers/palettes_controller.rb
+++ palette-swap-backend/app/controllers/palettes_controller.rb
@@ -1,22 +1,22 @@
 class PalettesController < ApplicationController
   def index
-    @palettes = Palette.all
-    render json: @palettes
+    palettes = Palette.all
+    render json: PaletteSerializer.new(palettes)
   end
 
   def create
-    @palette = Palette.new(palette_params)
+    palette = Palette.new(palette_params)
 
-    if @palette.save
-      render json: @palette, status: :created, location: @palette
+    if palette.save
+      render json: PaletteSerializer.new(palette)
     else
-      render json: @palette.errors, status: :unprocessable_entity
+      render json: palette.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @palette = Palette.find(params[:id])
-    @palette.destroy
+    palette = Palette.find(params[:id])
+    palette.destroy
   end
 
   private
--- palette-swap-backend/app/controllers/patterns_controller.rb
+++ palette-swap-backend/app/controllers/patterns_controller.rb
@@ -1,11 +1,11 @@
 class PatternsController < ApplicationController
   def index
-    @patterns = Pattern.all
-    render json: @patterns
+    patterns = Pattern.all
+    render json: PatternSerializer.new(patterns)
   end
 
   def show
-    @pattern = Pattern.find(params[:id])
-    render json: @pattern
+    pattern = Pattern.find(params[:id])
+    render json: PatternSerializer.new(pattern)
   end
 end
