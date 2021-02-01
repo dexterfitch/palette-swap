4e059dfcaecd5a35c619c187c4c5e90d636f9da2
Dex Fitch <fitch.dexter@gmail.com>
2021-01-31 16:00:11 -0800

    Set up CORS and initial fetch for Patterns

--- palette-swap-frontend/src/pattern.js
+++ palette-swap-frontend/src/pattern.js
@@ -1,6 +1,14 @@
 class Pattern {
+
   constructor(name, style) {
     this.name = name;
     this.style = style;
   }
+
+  getPatterns = () => {
+    fetch(PATTERNS_URL)
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
 }
